545591af2e7bad683a40436654bda63b37378b50
Carl Johnson <I don't use email>
2023-01-29 21:15:31 +0200

    fix(trash): escape node.absolute_path
    Before this commit, if absolute path of the node contained dollar or a
    double quote, trash failed with the error from the shell about quoting.

--- lua/nvim-tree/actions/fs/trash.lua
+++ lua/nvim-tree/actions/fs/trash.lua
@@ -53,7 +53,7 @@ function M.fn(node)
 
   -- trashes a path (file or folder)
   local function trash_path(on_exit)
-    vim.fn.jobstart(M.config.trash.cmd .. ' "' .. node.absolute_path .. '"', {
+    vim.fn.jobstart(M.config.trash.cmd .. ' "' .. node.absolute_path:gsub('(["$])', '"\'%1\'"') .. '"', {
       detach = true,
       on_exit = on_exit,
       on_stderr = on_stderr,
